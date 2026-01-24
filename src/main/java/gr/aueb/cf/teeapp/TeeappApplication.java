948a3b65d69af684ae2c5ac67bcd02908d356e70
dimitris418 <dimitris_matthioudakis@hotmail.com>
2026-01-24 20:40:17 +0200

    fix: add @JpaAuditing

--- src/main/java/gr/aueb/cf/teeapp/TeeappApplication.java
+++ src/main/java/gr/aueb/cf/teeapp/TeeappApplication.java
@@ -2,8 +2,10 @@ package gr.aueb.cf.teeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TeeappApplication {
 
 	public static void main(String[] args) {

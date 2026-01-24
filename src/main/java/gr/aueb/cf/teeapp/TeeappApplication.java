5129475d8e3a688a7266777ea75b3a3436fbc055
dimitris418 <dimitris_matthioudakis@hotmail.com>
2026-01-24 20:13:02 +0200

    feat: initial commit

---
+++ src/main/java/gr/aueb/cf/teeapp/TeeappApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.teeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/aueb/cf/teeapp/TeeappApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.teeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

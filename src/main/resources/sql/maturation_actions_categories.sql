0713c74d5796e0667945a3f394c87f7e4b180126
dimitris418 <dimitris_matthioudakis@hotmail.com>
2026-01-27 10:33:33 +0200

    feat: add maturation_actions_categories.sql data

---
+++ src/main/resources/sql/maturation_actions_categories.sql
@@ -0,0 +1,4 @@
+INSERT INTO maturation_actions_categories (id, name) VALUES
+(1, 'ΔΙΟΙΚΗΤΙΚΕΣ ΕΝΕΡΓΕΙΕΣ ΔΙΚΑΙΟΥΧΟΥ - ΩΡΙΜΑΝΣΗ'),
+(2, 'ΕΝΕΡΓΕΙΕΣ ΝΟΜΙΚΗΣ ΔΕΣΜΕΥΣΗΣ');
+ALTER TABLE maturation_actions_categories AUTO_INCREMENT = 3;

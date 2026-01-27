b17999ad2791e708604ad20015504a9036fd21d3
dimitris418 <dimitris_matthioudakis@hotmail.com>
2026-01-27 18:40:01 +0200

    feat (dto): add ConsultantReadOnlyDTO.java

---
+++ src/main/java/gr/aueb/cf/teeapp/dto/ConsultantReadOnlyDTO.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.teeapp.dto;
+
+import lombok.Builder;
+
+@Builder
+public record ConsultantReadOnlyDTO(
+        Long id,
+        String uuid,
+        Boolean isActive,
+        UserReadOnlyDTO userReadOnlyDTO,
+        PersonalInfoReadOnlyDTO personalInfoReadOnlyDTO) {}

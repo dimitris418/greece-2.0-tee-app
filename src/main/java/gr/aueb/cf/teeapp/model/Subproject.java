70c776f7cd0ad6815736077dbfd80b5a1d03e3f8
dimitris418 <dimitris_matthioudakis@hotmail.com>
2026-01-28 11:11:22 +0200

    fix (model): remove code from Subproject.java

--- src/main/java/gr/aueb/cf/teeapp/model/Subproject.java
+++ src/main/java/gr/aueb/cf/teeapp/model/Subproject.java
@@ -14,17 +14,4 @@ import lombok.Setter;
 @Table(name = "subprojects")
 public class Subproject {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "project_id")
-    private Project project;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "portfolio_id")
-    private Portfolio portfolio;
-
 }

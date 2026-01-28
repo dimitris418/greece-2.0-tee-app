5505e57277b2db5374ebff0dedbd3eb2ac911d6f
dimitris418 <dimitris_matthioudakis@hotmail.com>
2026-01-28 10:56:23 +0200

    feat (model): add Subproject.java

---
+++ src/main/java/gr/aueb/cf/teeapp/model/Subproject.java
@@ -0,0 +1,29 @@
+package gr.aueb.cf.teeapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@Table(name = "subprojects")
+public class Subproject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "portfolio_id")
+    private Portfolio portfolio;
+}

b12ff1d3332fba7ac2854638f5450eb74bad151a
dimitris418 <dimitris_matthioudakis@hotmail.com>
2026-02-06 13:06:32 +0200

    feat (repository): add MaturationActionRepository.java

---
+++ src/main/java/gr/aueb/cf/teeapp/repository/MaturationActionRepository.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.teeapp.repository;
+
+import gr.aueb.cf.teeapp.model.MaturationAction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MaturationActionRepository extends JpaRepository<MaturationAction, Long>,
+        JpaSpecificationExecutor<MaturationAction> {
+
+    Optional<MaturationAction> findById(String id);
+    Optional<MaturationAction> findByUuid(String uuid);
+    Optional<MaturationAction> findByReferenceNumber(String referenceNumber);
+    Optional<MaturationAction> findByRegulatoryAuthority(String regulatoryAuthority);
+    List<MaturationAction> findByStartDate(String startDate);
+    List<MaturationAction> findByEndDate(String endDate);
+    List<MaturationAction> findByDescription(String description);
+}

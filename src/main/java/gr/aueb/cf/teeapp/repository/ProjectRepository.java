39df820758864b89ef03e2cd81f4e2b6073da93b
dimitris418 <dimitris_matthioudakis@hotmail.com>
2026-02-06 10:26:07 +0200

    feat (repository): add ProjectRepository.java

---
+++ src/main/java/gr/aueb/cf/teeapp/repository/ProjectRepository.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.teeapp.repository;
+
+import gr.aueb.cf.teeapp.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface ProjectRepository extends JpaRepository<Project, Long>,
+        JpaSpecificationExecutor<Project> {
+}
+

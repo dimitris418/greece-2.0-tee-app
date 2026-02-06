e30e67c50ad83cb9541ac1f0e16931855ecd700b
dimitris418 <dimitris_matthioudakis@hotmail.com>
2026-02-06 14:55:29 +0200

    feat (dto): add ConsultantInsertDTO.java

---
+++ src/main/java/gr/aueb/cf/teeapp/dto/ConsultantInsertDTO.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.teeapp.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
+public record ConsultantInsertDTO(
+
+        @NotNull(message = "isActive field is required")
+        Boolean isActive,
+
+        @Valid
+        @NotNull(message = "User details are required")
+        UserInsertDTO userInsertDTO,
+
+        @Valid
+        @NotNull(message = "Personal Info is required")
+        PersonalInfoInsertDTO personalInfoInsertDTO
+) {}
+

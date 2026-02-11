ffbb2b90ef195e2de438259330bc16a293a73ed5
dimitris418 <dimitris_matthioudakis@hotmail.com>
2026-02-11 19:28:27 +0200

    feat (service): add AuthenticationService.java

---
+++ src/main/java/gr/aueb/cf/teeapp/authentication/AuthenticationService.java
@@ -0,0 +1,29 @@
+package gr.aueb.cf.teeapp.authentication;
+
+import gr.aueb.cf.teeapp.dto.AuthenticationRequestDTO;
+import gr.aueb.cf.teeapp.dto.AuthenticationResponseDTO;
+import gr.aueb.cf.teeapp.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponseDTO authenticate(AuthenticationRequestDTO dto) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(dto.username(), dto.password()));
+
+        User user = (User) authentication.getPrincipal();
+        String token = jwtService.generateToken(authentication.getName(), user.getRole().getName());
+        return new AuthenticationResponseDTO(user.getFirstname(), user.getLastname(), token);
+    }
+}

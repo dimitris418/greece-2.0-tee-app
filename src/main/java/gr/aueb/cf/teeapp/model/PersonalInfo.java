1c38754c8fc9db42b1015a890a69abf434b511ea
dimitris418 <dimitris_matthioudakis@hotmail.com>
2026-03-16 15:30:30 +0200

    feat (model): add PersonalInfo.java

---
+++ src/main/java/gr/aueb/cf/teeapp/model/PersonalInfo.java
@@ -0,0 +1,24 @@
+package gr.aueb.cf.teeapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "personal_information")
+public class PersonalInfo extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String identityNumber;
+
+    private String placeOfBirth;
+    private String municipalityOfRegistration;
+}

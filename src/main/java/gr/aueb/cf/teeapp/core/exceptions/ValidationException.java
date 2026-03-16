c721b7955f7b565f4c363f9adfd9dbd87c2f5bcd
dimitris418 <dimitris_matthioudakis@hotmail.com>
2026-03-16 16:08:32 +0200

    feat: add exceptions

---
+++ src/main/java/gr/aueb/cf/teeapp/core/exceptions/ValidationException.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.teeapp.core.exceptions;
+
+import lombok.Getter;
+import org.springframework.validation.BindingResult;
+
+@Getter
+public class ValidationException extends Exception {
+
+    private final BindingResult bindingResult;
+
+    public ValidationException(BindingResult bindingResult) {
+        super("Validation failed");
+        this.bindingResult = bindingResult;
+    }
+}

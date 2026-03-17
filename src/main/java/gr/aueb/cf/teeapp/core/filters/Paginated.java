209073554a12456972d5e9946bb1ab5c14c7a019
dimitris418 <dimitris_matthioudakis@hotmail.com>
2026-03-17 17:17:17 +0200

    feat: add filters

---
+++ src/main/java/gr/aueb/cf/teeapp/core/filters/Paginated.java
@@ -0,0 +1,32 @@
+package gr.aueb.cf.teeapp.core.filters;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+public class Paginated<T> {
+
+    List<T> data;
+    int currentPage;
+    int pageSize;
+    int totalPages;
+    int numberOfElements;
+    long totalElements;
+
+    public static <T> Paginated<T> fromPage(Page<T> page) {
+        return Paginated.<T>builder()
+                .data(page.getContent())
+                .currentPage(page.getNumber())
+                .pageSize(page.getSize())
+                .totalPages(page.getTotalPages())
+                .numberOfElements(page.getNumberOfElements())
+                .totalElements(page.getTotalElements())
+                .build();
+    }
+}

8b8f7881444850a5acbdc394277fd79509aba93b
dimitris418 <dimitris_matthioudakis@hotmail.com>
2026-04-02 15:05:04 +0300

    feat: enable JPA auditing with @EnableJpaAuditing in main application class

--- src/main/java/gr/aueb/cf/teeapp/TeeappApplication.java
+++ src/main/java/gr/aueb/cf/teeapp/TeeappApplication.java
@@ -2,12 +2,13 @@ package gr.aueb.cf.teeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TeeappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TeeappApplication.class, args);
 	}
-
 }

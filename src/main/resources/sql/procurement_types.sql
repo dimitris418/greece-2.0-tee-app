c1acba295da88409ecb2681f7600e322a8f6b7a8
dimitris418 <dimitris_matthioudakis@hotmail.com>
2026-04-02 12:31:28 +0300

    feat: add procurement_types.sql

---
+++ src/main/resources/sql/procurement_types.sql
@@ -0,0 +1,5 @@
+INSERT INTO procurement_types (id, name) VALUES
+(1, 'ΔΗΜΟΣΙΕΣ ΣΥΜΒΑΣΕΙΣ'),
+(2, 'ΠΡΟΣΚΛΗΣΕΙΣ ΕΝΙΣΧΥΣΕΩΝ'),
+(3, 'ΜΕΤΑΡΡΥΘΜΙΣΕΙΣ');
+ALTER TABLE procurement_types AUTO_INCREMENT = 4;
